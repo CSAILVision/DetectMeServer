b6f0528381e1f49645ccfbf01b737f972a8bed00
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2013-10-22 14:38:26 -0400

    Added debug toolbar to production

--- detectme/detectme/settings/production.py
+++ detectme/detectme/settings/production.py
@@ -81,3 +81,27 @@ DEBUG = True
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 #SECRET_KEY = get_env_setting('SECRET_KEY')
 ########## END SECRET CONFIGURATION
+
+
+
+########## TOOLBAR CONFIGURATION
+# NOTE: FOR DEBUGGING PURPOSES
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INTERNAL_IPS = ('127.0.0.1',)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
+########## END TOOLBAR CONFIGURATION

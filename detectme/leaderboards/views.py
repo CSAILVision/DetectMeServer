c33d823c2ab368920889462e20c7794fd459b241
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2014-01-06 12:02:57 -0500

    added bootstrap flavour to the leaderboard templates.

--- detectme/leaderboards/views.py
+++ detectme/leaderboards/views.py
@@ -11,9 +11,11 @@ def competition_detail(request):
 
 
 def show_leaderboard(request, category):
+    categories = Category.objects.all()
     category = Category.objects.get(name=category)
     teams = UserScore.objects.filter(category=category)
     teams = sorted(teams, key=lambda t: t.max_score, reverse=True)
     return render_to_response('leaderboards/leaderboard.html',
-                              {"teams": teams, "category": category},
+                              {"teams": teams, "category": category,
+                               "categories": categories},
                               context_instance=RequestContext(request))

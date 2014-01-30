0fb80b34470906ea886bdbd1763d32ba9262c6a4
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2014-01-29 21:52:22 -0500

    Added required login for own submissions. Draw with js the annotated box in the training image.

--- detectme/leaderboards/views.py
+++ detectme/leaderboards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from detectors.models import Detector
 from .models import Category, UserScore, Performance
 
 
@@ -18,9 +18,9 @@ def show_leaderboard(request, category):
                               {'teams': teams, 'selected_item': category.name},
                               context_instance=RequestContext(request))
 
-
+@login_required
 def submissions(request):
-    user = request.user
+    user = request.user.get_profile()
     performances = Performance.objects.filter(detector_author=user)
     return render_to_response('leaderboards/submissions.html',
                               {'performances': performances, 'selected_item': "submissions"},
